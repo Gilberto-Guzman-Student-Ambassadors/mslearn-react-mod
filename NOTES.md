9d504f58337062544f2e1c4d576abb4d72093b6f
Gilberto-Guzman-Student-Ambassadors <165218190+Gilberto-Guzman-Student-Ambassadors@users.noreply.github.com>
2024-05-30 22:45:53 +0000

    Actualizacion del contenido

--- NOTES.md
+++ NOTES.md
@@ -3,7 +3,17 @@
     git https://github.com/Gilberto-Guzman-Student-Ambassadors/mslearn-react-mod
     cd mslearn-react-mod/code/0-starter
 
+    npm install -g npm-check-updates
+    ncu
+    ncu -u
     npm install
     npm audit fix --force
+
+    npm start
+
+
+    npm install
+    npm audit fix --force
+
 
     code .
---
+++ code/0-starter/src/index.jsx
@@ -0,0 +1,10 @@
+// Esta biblioteca nos permite utilizar jsx
+import React from 'react';
+
+// Esta biblioteca nos permite representar nuestra aplicación dentro del codigo html
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <h1>Hello, world!</h1>
+);
